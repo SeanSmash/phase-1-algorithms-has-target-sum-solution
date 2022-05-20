6314ec95adafebf9d218720cc16d43dc03077b80
Sean Sarokas <sps4462@gmail.com>
2022-05-20 05:35:10 -0400

    commit tree a7268ceb2d16e0eab42a94623e6b667367e09d7b

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const numberSet = {}
+  for (const number of array){
+    const complement = target - number
+    if(complement in numberSet) return true
+    numberSet[number] = true
+  }
+  return false
 }
 
 /* 
@@ -8,10 +15,19 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  function hasTargetSum(array, target){
+    const numberSet = {}
+    for (const number of array){
+      if it's the complement
+    }
+  }
 */
 
 /*
   Add written explanation of your solution here
+  create a function that takes a set of numbers and a target number 
+  and determines if there are two numbers whose sum equals the target 
+  number, and return true
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +45,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 5, 33, -11], 22));
 }
 
 module.exports = hasTargetSum;
